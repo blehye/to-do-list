62f87b77ea63eb5a177e8306ffc379b3f1719c24
blehye <hhw0850@naver.com>
2022-08-13 21:09:09 +0900

    커밋
    시계, 인용구, 투두리스트 추가,삭제

---
+++ js/todo.js
@@ -0,0 +1,39 @@
+const toDoForm = document.querySelector("#todo-form");
+
+const toDoInput = toDoForm.querySelector("input");
+
+const toDoList = document.querySelector("#todo-list");
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paintToDo(newTodo);
+  saveToDos();
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
+  toDoList.appendChild(li);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
